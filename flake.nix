5eedfb64624cdcd4d479cfe042dcf5769400bf59
Michael Adler <therisen06@gmail.com>
2021-07-25 23:45:19 +0200

    nix: use naersk

--- flake.nix
+++ flake.nix
@@ -1,21 +1,44 @@
 {
-  description = "A flake for shell-snoop";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+    naersk = {
+      url = "github:nmattia/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
-  outputs = { self, nixpkgs, flake-utils }:
-
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
+  outputs = { self, nixpkgs, utils, naersk }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
+        naersk-lib = naersk.lib."${system}";
+      in
       rec {
+        # `nix build`
+        packages.shell-snoop = naersk-lib.buildPackage {
+          pname = "shell-snoop";
+          version = "0.3.0";
+          root = ./.;
 
-        packages = {
-          shell-snoop = pkgs.callPackage ./default.nix { };
+          meta = with pkgs.lib; {
+            homepage = "https://github.com/michaeladler/shell-snoop";
+            description = "figure out the exact command which was used to run a child process in a shell";
+            platforms = platforms.linux;
+            license = licenses.asl20;
+          };
         };
-
         defaultPackage = packages.shell-snoop;
 
-      });
+        # `nix run`
+        apps.shell-snoop = utils.lib.mkApp {
+          drv = packages.shell-snoop;
+        };
+        defaultApp = apps.shell-snoop;
 
+        # `nix develop`
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ rustc cargo ];
+        };
+      });
 }
