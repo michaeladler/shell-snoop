5fe3eca1f8e6ecaea1655d599700c30089d66efc
Michael Adler <therisen06@gmail.com>
2022-06-29 22:30:20 +0200

    refactor: proc_status -> libproc

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::process::{self, Stdio};
 
 use anyhow::Result;
 use caps::{self, CapSet, Capability};
-use proc_status::{ProcRef, ProcStatus};
+use libproc::libproc::proc_pid;
 use rev_lines::RevLines;
 
 fn main() -> Result<()> {
@@ -14,7 +14,7 @@ fn main() -> Result<()> {
 
     let args = env::args();
     for pid in args.skip(1) {
-        let pid: usize = pid.parse::<usize>()?;
+        let pid: i32 = pid.parse::<i32>()?;
 
         let contents = fs::read_to_string(format!("/proc/{}/task/{}/children", pid, pid))?;
         if contents.is_empty() {
@@ -23,57 +23,72 @@ fn main() -> Result<()> {
             continue;
         }
 
-        let dumpfile = format!("/tmp/shell-history-{}.txt", pid);
-
-        let status = ProcStatus::read_for(ProcRef::ProcId(pid))?;
-        let is_zsh = matches!(status.value("Name"), Ok("zsh"));
-        let dump_cmd = if is_zsh {
-            format!("call (void)savehistfile(\"{}\", 0, 0)", &dumpfile)
-        } else {
-            format!("call (void)write_history(\"{}\")", &dumpfile)
+        let shell = match proc_pid::name(pid) {
+            Ok(name) => match name.as_str() {
+                "zsh" => Some(Shell::Zsh),
+                "bash" => Some(Shell::Bash),
+                _ => None,
+            },
+            _ => None,
         };
 
-        let mut cmd = process::Command::new("gdb")
-            .stdin(Stdio::null())
-            .stdout(Stdio::null())
-            .arg("-n")
-            .arg("-batch")
-            .arg("--eval")
-            .arg(format!("attach {}", pid))
-            .arg("--eval")
-            .arg(dump_cmd)
-            .arg("--eval")
-            .arg("detach")
-            .arg("--eval")
-            .arg("q")
-            .spawn()?;
-        cmd.wait()?;
+        if let Some(shell) = shell {
+            let dumpfile = format!("/tmp/shell-history-{}.txt", pid);
+            let mut cmd = process::Command::new("gdb")
+                .stdin(Stdio::null())
+                .stdout(Stdio::null())
+                .arg("-n")
+                .arg("-batch")
+                .arg("--eval")
+                .arg(format!("attach {}", pid))
+                .arg("--eval")
+                .arg(shell.dumpcmd(&dumpfile))
+                .arg("--eval")
+                .arg("detach")
+                .arg("--eval")
+                .arg("q")
+                .spawn()?;
+            cmd.wait()?;
 
-        match File::open(&dumpfile) {
-            Ok(file) => {
-                let reader = BufReader::new(file);
-                let mut rev_lines = RevLines::new(reader)?;
-                if let Some(last_line) = rev_lines.next() {
-                    let cmd = if is_zsh {
-                        last_line
-                            .split(';')
-                            .nth(1)
-                            .expect("Unable to determine command")
-                    } else {
-                        &last_line
-                    };
-                    println!("{}", cmd);
-                }
+            match File::open(&dumpfile) {
+                Ok(file) => {
+                    let reader = BufReader::new(file);
+                    let mut rev_lines = RevLines::new(reader)?;
+                    if let Some(last_line) = rev_lines.next() {
+                        let cmd = match shell {
+                            Shell::Zsh => last_line
+                                .split(';')
+                                .nth(1)
+                                .expect("Unable to determine command"),
+                            Shell::Bash => &last_line,
+                        };
+                        println!("{}", cmd);
+                    }
 
-                if let Err(e) = std::fs::remove_file(&dumpfile) {
-                    eprintln!("Failed to remove dump file: {}", e);
+                    if let Err(e) = std::fs::remove_file(&dumpfile) {
+                        eprintln!("Failed to remove dump file: {}", e);
+                    }
                 }
-            }
-            Err(e) => {
-                eprintln!("Error reading {}: {}", dumpfile, e);
+                Err(e) => {
+                    eprintln!("Error reading {}: {}", dumpfile, e);
+                }
             }
         }
     }
 
     Ok(())
+}
+
+enum Shell {
+    Bash,
+    Zsh,
+}
+
+impl Shell {
+    pub fn dumpcmd(&self, dumpfile: &str) -> String {
+        match self {
+            Shell::Zsh => format!("call (void)savehistfile(\"{}\", 0, 0)", &dumpfile),
+            Shell::Bash => format!("call (void)write_history(\"{}\")", &dumpfile),
+        }
+    }
 }
