0d3ef94e026d9f8c39ee00ad15681943b62b5826
Michael Adler <therisen06@gmail.com>
2022-10-13 09:23:32 +0200

    chore: update deps

--- flake.nix
+++ flake.nix
@@ -1,4 +1,6 @@
 {
+  description = "Build a cargo project";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
@@ -8,14 +10,21 @@
     };
 
     flake-utils.url = "github:numtide/flake-utils";
+
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, crane, flake-utils, ... }:
+  outputs = { self, nixpkgs, crane, flake-utils, advisory-db, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
         };
+
+        inherit (pkgs) lib;
 
         craneLib = crane.lib.${system};
         src = ./.;
@@ -28,14 +37,14 @@
 
         # Build the actual crate itself, reusing the dependency
         # artifacts from above.
-        shell-snoop = craneLib.buildPackage {
+        my-crate = craneLib.buildPackage {
           inherit cargoArtifacts src;
         };
       in
       {
         checks = {
           # Build the crate as part of `nix flake check` for convenience
-          inherit shell-snoop;
+          inherit my-crate;
 
           # Run clippy (and deny all warnings) on the crate source,
           # again, resuing the dependency artifacts from above.
@@ -43,29 +52,45 @@
           # Note that this is done as a separate derivation so that
           # we can block the CI if there are issues here, but not
           # prevent downstream consumers from building our crate by itself.
-          shell-snoop-clippy = craneLib.cargoClippy {
+          my-crate-clippy = craneLib.cargoClippy {
             inherit cargoArtifacts src;
-            cargoClippyExtraArgs = "-- --deny warnings";
+            cargoClippyExtraArgs = "--all-targets -- --deny warnings";
           };
 
           # Check formatting
-          shell-snoop-fmt = craneLib.cargoFmt {
+          my-crate-fmt = craneLib.cargoFmt {
             inherit src;
           };
 
+
+          # Audit dependencies
+          my-crate-audit = craneLib.cargoAudit {
+            inherit src advisory-db;
+          };
+
+          # Run tests with cargo-nextest
+          # Consider setting `doCheck = false` on `my-crate` if you do not want
+          # the tests to run twice
+          my-crate-nextest = craneLib.cargoNextest {
+            inherit cargoArtifacts src;
+            partitions = 1;
+            partitionType = "count";
+          };
+        } // lib.optionalAttrs (system == "x86_64-linux") {
+          # NB: cargo-tarpaulin only supports x86_64 systems
           # Check code coverage (note: this will not upload coverage anywhere)
-          shell-snoop-coverage = craneLib.cargoTarpaulin {
+          my-crate-coverage = craneLib.cargoTarpaulin {
             inherit cargoArtifacts src;
           };
         };
 
-        packages.default = shell-snoop;
+        packages.default = my-crate;
 
         apps.default = flake-utils.lib.mkApp {
-          drv = shell-snoop;
+          drv = my-crate;
         };
 
-        devShell = pkgs.mkShell {
+        devShells.default = pkgs.mkShell {
           inputsFrom = builtins.attrValues self.checks;
 
           # Extra inputs can be added here
